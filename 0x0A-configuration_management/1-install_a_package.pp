20fb8926e09786c5ecb8ba4a1f383c47cf28b9b9
LuisPatino92 <2450@holbertonschool.com>
2021-04-02 21:31:22 +0000

    Style fixed to met linter requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #installs puppet-lint in its 2.1.1 version
 package {'puppet-lint':
-  name            => 'puppet-lint',
   ensure          => installed,
+  name            => 'puppet-lint',
   install_options => ['-v', '2.1.1'],
 }
