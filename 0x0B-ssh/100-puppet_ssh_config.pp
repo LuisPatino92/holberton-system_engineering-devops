1ea0e733261d03300b6892be25e292988dbbb9cd
LuisPatino92 <2450@holbertonschool.com>
2021-04-03 21:28:15 +0000

    Using file_lineII

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 # Ensures that there is a holberton file in tmp
 file_line { '/etc/ssh/ssh_config':
   ensure => 'present',
-  line   => '	PasswordAuthentication no'
+  path   => '/etc/ssh/ssh_config',
+  line   => '	IdentityFile ~/.ssh/holberton',
 }
-file_line { '/etc/ssh/ssh_config':
+file_line { '/etc/ssh/ssh_configII':
   ensure => 'present',
-  line   => '	IdentityFile ~/.ssh/holberton'
+  path   => '/etc/ssh/ssh_config',
+  line   => '	PasswordAuthentication no',
 }
