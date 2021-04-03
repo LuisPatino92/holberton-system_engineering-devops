1175f959292720e4495a0f67b1289f257bf74f27
LuisPatino92 <2450@holbertonschool.com>
2021-04-03 18:08:56 +0000

    Style fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,10 +2,12 @@
 file {'/etc/ssh/ssh_config':
   ensure  => file,
   mode    => '0600',
-  content => 
+  content =>
   "# Read more about SSH config files: https://linux.die.net/man/5/ssh_config\nHost LuchoServer
-  	HostName 35.196.27.222
-	User ubuntu\n\tPort 22
+	HostName 35.196.27.222
+	User ubuntu
+	Port 22
 	IdentityFile ~/.ssh/holberton
-	PasswordAuthentication no",
+	PasswordAuthentication no
+",
 }
