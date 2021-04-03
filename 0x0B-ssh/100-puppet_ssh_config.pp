77fcf217d19333defc413bd6f56a3a5101d7c1df
LuisPatino92 <2450@holbertonschool.com>
2021-04-03 21:26:28 +0000

    Using file_line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,9 @@
-#Ensures that there is a holberton file in tmp
-file {'/etc/ssh/ssh_config':
-  ensure  => file,
-  mode    => '0600',
-  content =>
-  "# Read more about SSH config files: https://linux.die.net/man/5/ssh_config\nHost LuchoServer
-  HostName 35.196.27.222
-  User ubuntu
-  Port 22
-  IdentityFile ~/.ssh/holberton
-  PasswordAuthentication no
-",
+# Ensures that there is a holberton file in tmp
+file_line { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  line   => '	PasswordAuthentication no'
+}
+file_line { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  line   => '	IdentityFile ~/.ssh/holberton'
 }
