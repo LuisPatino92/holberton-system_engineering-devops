efe16594963293d2f5f8e633865cc91f37ba425f
LuisPatino92 <2450@holbertonschool.com>
2021-04-03 18:01:27 +0000

    Using double quotes to indicate multiline file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,10 @@
 file {'/etc/ssh/ssh_config':
   ensure  => file,
   mode    => '0600',
-  content => '# Read more about SSH config files: https://linux.die.net/man/5/ssh_config\nHost LuchoServer\n\tHostName 35.196.27.222\n\tUser ubuntu\n\tPort 22\n\tIdentityFile ~/.ssh/holberton\n\tPasswordAuthentication no\n',
+  content => 
+  "# Read more about SSH config files: https://linux.die.net/man/5/ssh_config\nHost LuchoServer
+  	HostName 35.196.27.222
+	User ubuntu\n\tPort 22
+	IdentityFile ~/.ssh/holberton
+	PasswordAuthentication no",
 }
