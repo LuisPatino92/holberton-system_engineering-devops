a8c3202c8f5fd5780326e4714e9af6fa8deb86e7
LuisPatino92 <2450@holbertonschool.com>
2021-04-04 15:44:14 +0000

    Present instead of file in ensure atribute III

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # Ensures that there is a holberton file in tmp
+
 file { '/tmp/holberton':
-  ensure  => file,
+  ensure  => present,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
